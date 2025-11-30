f18db42c15fbe1916ddc75f2c4aa61d2ea1da736
Omar <hroumtiomar11@gmail.com>
2025-11-30 02:22:40 +0100

    Revert "Merge branch 'master' of https://github.com/iMex-1/3d-hustle"
    This reverts commit b55e7b1716251fcc8f09dd8d4fac5e585cff1546, reversing
    changes made to 7d82ee236951d877f5571bd07617b07271999037.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './styles/theme.css'
 import './styles/main.css'
 import './styles/animations.css'
-import './styles/lighter-theme.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
